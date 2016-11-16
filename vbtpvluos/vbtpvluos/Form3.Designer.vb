df6f912bf0f9edd865daed7743333656b07c9379
antolin <andresito-93@live.com>
2016-11-16 20:53:30 +0100

    biblio de verificacion

--- vbtpvluos/vbtpvluos/Form3.Designer.vb
+++ vbtpvluos/vbtpvluos/Form3.Designer.vb
@@ -26,11 +26,14 @@ Partial Class Form3
         Me.Label1 = New System.Windows.Forms.Label()
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(148, 93)
+        Me.TextBox1.Location = New System.Drawing.Point(148, 96)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(183, 22)
         Me.TextBox1.TabIndex = 0
@@ -64,11 +67,39 @@ Partial Class Form3
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "Categoría:"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(148, 267)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 48)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Guardar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(49, 144)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 17)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Precio:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(148, 144)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(183, 22)
+        Me.TextBox2.TabIndex = 6
+        '
         'Form3
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(373, 348)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.ComboBox1)
         Me.Controls.Add(Me.Label1)
@@ -83,4 +114,7 @@ Partial Class Form3
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
     Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
 End Class
--- vbtpvluos/vbtpvluos/Form3.vb
+++ vbtpvluos/vbtpvluos/Form3.vb
@@ -1,3 +1,18 @@
-﻿Public Class Form3
+﻿Imports ClassLibrary1
 
+Public Class Form3
+    Dim validar As New ClassLibrary1.Class1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_Validated(sender As Object, e As EventArgs) Handles TextBox1.Validated
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If validar.validar(TextBox1.Text) = False Then
+            MsgBox("Error")
+        End If
+    End Sub
 End Class
